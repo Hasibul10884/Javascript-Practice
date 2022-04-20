7d5686a8da52b175c1053199bdbde0d58e5df70d
Hasibul10884 <hasibulhasan193@gmail.com>
2022-04-20 19:33:47 +0600

    bvbv

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 console.log('Hello World');
-document.write("DDDDDDDDDDDDDDDD");
+document.write("Hello World");
+
+
+//User Input;
+var x;
+x= prompt("Enter your name:");
+console.log(x);
+document.write(x);
+
+
